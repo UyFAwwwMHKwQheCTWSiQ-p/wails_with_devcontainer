fce6f3f2ccf81653f046d761dbf11e3bd9dc1040
Mizutani <flipper-twines0s@icloud.com>
2024-05-11 04:25:16 +0900

    wails with devcontainer

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,4 @@
+echo 'source /home/vscode/.oh-my-zsh/plugins/gitfast/git-completion.bash' >> ~/.bashrc
+git config --global --add safe.directory /workspace
+sudo chown -R $USER:$USER /go
+echo "(sudo Xvfb :99 -screen 0 1024x768x24 &> /dev/null &) & disown" >> ~/.bashrc
