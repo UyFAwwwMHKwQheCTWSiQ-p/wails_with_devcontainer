81d3cb292eeea76fbe04eea05cd3f4bda2bfadfc
UyFAwwwMHKwQheCTWSiQ-p <140072249+UyFAwwwMHKwQheCTWSiQ-p@users.noreply.github.com>
2024-06-11 00:34:31 +0900

    post-create.shの内容を更新
    起動時にnpminstallを実行するように変更

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,3 +2,4 @@ echo 'source /home/vscode/.oh-my-zsh/plugins/gitfast/git-completion.bash' >> ~/.
 git config --global --add safe.directory /workspace
 sudo chown -R $USER:$USER /go
 echo "(sudo Xvfb :99 -screen 0 1024x768x24 &> /dev/null &) & disown" >> ~/.bashrc
+npm install
